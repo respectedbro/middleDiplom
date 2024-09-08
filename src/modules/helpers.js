776702e70827bf14bfe0af536e4585648aeb29b6
NikDm <respectedoff@yandex.ru>
2024-09-08 21:36:21 +0300

    timer рудÐhelpers pr

--- src/modules/helpers.js
+++ src/modules/helpers.js
@@ -1,6 +1,6 @@
 const animate = ({timing, draw, duration}) => {
 
-    let start = performance.now();
+    const start = performance.now();
 
     requestAnimationFrame(function animate(time) {
         // timeFraction изменяется от 0 до 1
@@ -8,7 +8,7 @@ const animate = ({timing, draw, duration}) => {
         if (timeFraction > 1) timeFraction = 1;
 
         // вычисление текущего состояния анимации
-        let progress = timing(timeFraction);
+        const progress = timing(timeFraction);
 
         draw(progress); // отрисовать её
 
--- src/modules/timer.js
+++ src/modules/timer.js
@@ -11,13 +11,13 @@ export const timer = (deadline) => {
         let timeout
 
         const getTimeRemaining = () => {
-            let dateStop = new Date(deadline).getTime()
-            let dateNow = new Date().getTime()
-            let timeRemaining = (dateStop - dateNow) / 1000
-            let days = Math.floor(timeRemaining / 60 / 60 / 24)
-            let hours = Math.floor((timeRemaining / 60 / 60) % 24)
-            let minutes = Math.floor((timeRemaining / 60) % 60)
-            let seconds = Math.floor(timeRemaining % 60)
+            const dateStop = new Date(deadline).getTime()
+            const dateNow = new Date().getTime()
+            const timeRemaining = (dateStop - dateNow) / 1000
+            const days = Math.floor(timeRemaining / 60 / 60 / 24)
+            const hours = Math.floor((timeRemaining / 60 / 60) % 24)
+            const minutes = Math.floor((timeRemaining / 60) % 60)
+            const seconds = Math.floor(timeRemaining % 60)
 
             return {timeRemaining, days, hours, minutes, seconds}
         }
@@ -31,7 +31,7 @@ export const timer = (deadline) => {
         }
 
         const updateClock = () => {
-            let getTime = getTimeRemaining()
+            const getTime = getTimeRemaining()
             timerDays.textContent = addTimeZero(getTime.days)
             timerHours.textContent = addTimeZero(getTime.hours)
             timerMinutes.textContent = addTimeZero(getTime.minutes)
