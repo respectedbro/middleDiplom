7dd1307003c8815cf80f025f276557c0bb209cb5
resAsus <respectedbro@yandex.ru>
2024-09-10 20:23:12 +0300

    test1

--- src/modules/render.js
+++ src/modules/render.js
@@ -6,18 +6,13 @@ const commentsContainer = document.querySelector('.comments-container')
         <div class="review-margin-bottom row comment-item">
                     <div class="col-xs-3 col-sm-2">
                         <div class="review-user">
-                            <img src="./images/users/face4.png" alt="" class="img-responsive avatar">
+                            <img src="./images/users/${user.image}" alt="" class="img-responsive avatar">
                         </div>
                     </div>
                     <div class="col-xs-9 col-sm-9">
                         <div class="review-inner review-green review-arrow review-arrow-left">
-                            <p class="text-normal">Алексей Панкратов</p>
-                            <p>Не люблю писать отзывы, но в данном случае решил помочь компании, потому что неоднократно
-                                обращался в другие организации со своими просьбами, но либо не было компетентного
-                                сотрудника, либо навязывали стандартные решения. У меня коттедж, и мне нужны были
-                                нестандартные цветные окна, чтобы не нарушать дизайн интерьера моего дома. Еще один
-                                момент, после обращения, ко мне загород бесплатно отправили мастера для обсуждения моего
-                                заказа. Парень отработал на отлично, помог с решением технических моментов!</p>
+                            <p class="text-normal">${user.author}</p>
+                            <p>${user.comment}</p>
                         </div>
                     </div>
                 </div>
